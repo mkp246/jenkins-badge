f9a6f11f88b68b2e197c3d6c8572a628d91c4456
mkp246 <mkp246@gmail.com>
2018-07-23 21:30:50 +0530

    add config file to load debug and ssl options

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
+const fs = require('fs');
+
+fs.readFileSync('config/app.properties').toString().split('\n').forEach(line => {
+    keyVal = line.split("=", 2);
+    process.env[keyVal[0]] = keyVal[1];
+});
+
 const express = require('express');
 const dbg = require('debug')('app');
 const app = express();
 const jenkinsapi = require('jenkins-api');
 const https = require('https');
-const fs = require('fs');
 
-var jenkins = jenkinsapi.init("https://nnmjenkins.ftc.hpeswlab.net:8443/jenkins");
+const jenkins = jenkinsapi.init("https://nnmjenkins.ftc.hpeswlab.net:8443/jenkins");
 
-var status = require("./status")(dbg, jenkins);
+const status = require("./status")(dbg, jenkins);
 app.use("/status", status);
 
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
-var sslOptions = {
+
+const sslOptions = {
     key: fs.readFileSync('certs/server.key'),
     cert: fs.readFileSync('certs/server.crt')
 };
