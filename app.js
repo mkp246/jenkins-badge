4e2be3c4a366804755fbf8ec828b98144ba0f77d
mkp246 <mkp246@gmail.com>
2018-07-31 01:38:42 +0530

    read config file line by line to avoid memory leak, use promise to sync code further

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
 const fs = require('fs');
+const readline = require('readline');
 
-fs.readFileSync('config/app.properties').toString().split('\n').forEach(line => {
-    var [key, val] = line.split("=", 2);
-    process.env[key] = val;
+let done = new Promise(resolve => {
+    readline.createInterface(fs.createReadStream('./config/app.properties'))
+        .on('line', line => {
+            var [key, val] = line.split("=", 2);
+            process.env[key] = val;
+        })
+        .on('close', () => {
+            resolve(0);
+        });
 });
+done.then(() => {
+    const express = require('express');
+    const dbg = require('debug')('app');
+    const https = require('https');
+    const jenkinsApi = require('./lib/jenkinsApi')(https);
+    const app = express();
 
-const express = require('express');
-const dbg = require('debug')('app');
-const https = require('https');
-const jenkinsApi = require('./lib/jenkinsApi')(https);
-const app = express();
+    const jenkins = jenkinsApi.init("nnmjenkins.ftc.hpeswlab.net", 8443, "jenkins");
 
-const jenkins = jenkinsApi.init("nnmjenkins.ftc.hpeswlab.net", 8443, "jenkins");
+    const status = require("./api/status")(dbg, jenkins);
+    app.use("/status", status);
 
-const status = require("./api/status")(dbg, jenkins);
-app.use("/status", status);
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+
+    const sslOptions = {
+        key: fs.readFileSync('certs/server.key'),
+        cert: fs.readFileSync('certs/server.crt')
+    };
+    https.createServer(sslOptions, app).listen(8888);
+    dbg('server started ...');
 });
-
-
-const sslOptions = {
-    key: fs.readFileSync('certs/server.key'),
-    cert: fs.readFileSync('certs/server.crt')
-};
-https.createServer(sslOptions, app).listen(8888);
-dbg('server started ...');
