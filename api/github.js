2f50cfa72827ef0792d08acaeaa99bb42d8879cf
mkp246 <mkp246@gmail.com>
2018-08-13 19:58:53 +0530

    add contributor and legend badge(perf impact bcz of bad GihHub api),add header support in req lib, fix joun data chuck bug in request lib, add exclude legends propery and doc in props

--- api/github.js
+++ api/github.js
@@ -4,6 +4,16 @@ const dbg = require('debug')('api:github:');
 const badge = require('../badge/badge');
 const normal = require('../badge/template/normal');
 
+const testBadge = require('../badge/testBadge');
+const test = require('../badge/template/test');
+
+const config = {};
+config.legendsExclude = {};
+process.env.LEGENDS_EXCLUDE.split(',').forEach(val => {
+    config.legendsExclude[val] = 0;
+});
+dbg(config);
+
 github.get("/:repo/commits", (req, res) => {
     let repo = req.params.repo;
     dbg("repo: %s", repo);
@@ -63,6 +73,47 @@ github.get("/:repo/tags", (req, res) => {
             status: tags,
             color: 'blue',
         }, normal);
+        res.set('Content-Type', 'image/svg+xml');
+        res.send(svg);
+    });
+})
+
+github.get("/:repo/contributors", (req, res) => {
+    let repo = req.params.repo;
+    dbg("repo: %s", repo);
+    gitHubApi.getContributors(repo, (err, data) => {
+        let contributorCount = data.length.toString();
+        let svg = badge({
+            subject: 'contributors',
+            status: contributorCount,
+            color: 'blue',
+        }, normal);
+        res.set('Content-Type', 'image/svg+xml');
+        res.send(svg);
+    });
+})
+
+github.get("/:repo/legends", (req, res) => {
+    let repo = req.params.repo;
+    dbg("repo: %s", repo);
+    gitHubApi.getLegends(repo, (err, data) => {
+        data = data.slice(-5);
+        let legends = [];
+        let dataLength = data.length - 1;
+        for (let index = dataLength; index > -1; index--) {
+            let login = data[index].author.login;
+            if (!(login in config.legendsExclude)) legends.push(login);
+        }
+        dbg(legends);
+        let svg = testBadge({
+            subject: 'legends',
+            status1: legends[0] || "N/A",
+            color1: 'blue',
+            status2: legends[1] || "N/A",
+            color2: 'yellow',
+            status3: legends[2] || "N/A",
+            color3: 'red',
+        }, test);
         res.set('Content-Type', 'image/svg+xml');
         res.send(svg);
     });
