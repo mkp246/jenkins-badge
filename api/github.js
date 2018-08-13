94eb3dae15e7fc41c0003444c653b82ee1556e84
mkp246 <mkp246@gmail.com>
2018-08-13 14:33:31 +0530

    convert callback from fuction to lambda, implement path traversal and getter from json, reformat commit count to youtube style view count eg 1.6M, 543K, 983, but add remaining less signifcant part also,implemet branch count

--- api/github.js
+++ api/github.js
@@ -4,7 +4,7 @@ const dbg = require('debug')('api:github:');
 const badge = require('../badge/badge');
 const normal = require('../badge/template/normal');
 
-github.get("/:repo/commits", function(req, res) {
+github.get("/:repo/commits", (req, res) => {
     let repo = req.params.repo;
     dbg("repo: %s", repo);
     gitHubApi.getCommitCount(repo, (err, data) => {
@@ -14,9 +14,36 @@ github.get("/:repo/commits", function(req, res) {
         }
         dbg(data);
         let json = JSON.parse(data);
+        let commits = (getPathOrNull(json, "data.repository.defaultBranchRef.target.history.totalCount") || 0).toString();
+        let commitsHuman;
+        let commitsLength = commits.length;
+        if (commitsLength <= 3) {
+            commitsHuman = commits;
+        } else if (commits.length <= 6) {
+            commitsHuman = commits.substr(0, commitsLength - 3) + "K" + commits.substr(-3);
+        } else {
+            commitsHuman = commits.substr(0, commitsLength - 6) + "." + commits.substr(-6, 1) + "M" + commits.substr(-5, 2) + "K" + commits.substr(-3);
+        }
         let svg = badge({
             subject: 'commits',
-            status: (getPathOrNull(json, "data.repository.defaultBranchRef.target.history.totalCount") || 0).toString(),
+            status: commitsHuman,
+            color: 'blue',
+        }, normal);
+        res.set('Content-Type', 'image/svg+xml');
+        res.send(svg);
+    });
+})
+
+github.get("/:repo/branches", (req, res) => {
+    let repo = req.params.repo;
+    dbg("repo: %s", repo);
+    gitHubApi.getBranchCount(repo, (err, data) => {
+        dbg(data);
+        let json = JSON.parse(data);
+        let branches = (getPathOrNull(json, "data.repository.refs.totalCount") || 0).toString();
+        let svg = badge({
+            subject: 'branches',
+            status: branches,
             color: 'blue',
         }, normal);
         res.set('Content-Type', 'image/svg+xml');
